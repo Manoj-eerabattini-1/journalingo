ba1b1d52e3c13d18bccca564d07653484b9b14c0
Manoj <eerabattinimanoj1@gmail.com>
2026-03-30 23:51:05 +0530

    Rendering individual cards or journal entries

--- frontend/src/Components/FullEntry.jsx
+++ frontend/src/Components/FullEntry.jsx
@@ -1,18 +1,28 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export function FullEntry() {
+    const { id } = useParams(); 
     const [ entry, setEntry ] = useState("");
 
-    const fetchEntry = () => {
-        fetch("http://localhost:3000//entry/:id")
+    useEffect(() => {
+        fetch(`http://localhost:3000/entry/${id}`)
         .then(res => res.json())
-        .then(data => setEntry(data))
+        .then(data => {
+            console.log(data);
+            setEntry(data);
+        })
         .catch(err => console.log(err));
-    }
+    }, [id]);
+
+    if(!entry) return <div>....Loading</div>
+
 
     return (
-        <div>
-            
+        <div className="w-[95%] mx-auto mt-5 bg-blue-100 p-6 rounded-lg">
+            <h2 className="text-2xl font-semibold mb-2">Entry on {new Date(entry.createdAt).toLocaleString("pt-BR")}</h2>
+            <br></br>
+            <p className="whitespace-pre-line">{entry.content}</p>
         </div>
     );
 }
